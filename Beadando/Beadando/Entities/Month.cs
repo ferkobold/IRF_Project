4ef90585c964ecbf3447b972aac2ed052fbcfaf2
ferkobold <barta.ferenc98@gmail.com>
2020-12-06 14:16:15 +0100

    Month Enumeration

---
+++ Beadando/Beadando/Entities/Month.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando.Entities
+{
+    public enum Month
+    {
+        Január = 1, 
+        Február = 2, 
+        Március = 3, 
+        Április = 4, 
+        Május = 5, 
+        Június = 6, 
+        Július = 7, 
+        Augusztus = 8, 
+        Szeptember = 9, 
+        Október = 10, 
+        November = 11, 
+        December = 12
+    }
+}

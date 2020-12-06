df0ad8ac599f67e7463ea5dbb0bafe0ca5c1fc03
ferkobold <barta.ferenc98@gmail.com>
2020-12-06 14:57:15 +0100

    Adding Income and Expense types as enums

---
+++ Beadando/Beadando/Entities/Expensetype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando.Entities
+{
+    public enum Expensetype
+    {
+        Közvetlen = 1,
+        Pénzügyi = 2,
+        Adminisztratív = 3,
+        Értékesítési = 4,
+        Egyéb = 5
+    }
+}
---
+++ Beadando/Beadando/Entities/Incometype.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando.Entities
+{
+    public enum Incometype
+    {
+        Értékesítés = 1,
+        Pénzügyi = 2,
+        Egyéb = 3
+    }
+}

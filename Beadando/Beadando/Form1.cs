c12e7538dac27c1936e81985a137250e7c0a551f
ferkobold <barta.ferenc98@gmail.com>
2020-12-06 15:12:53 +0100

    Making lists for Expenses and Incomes

--- Beadando/Beadando/Form1.cs
+++ Beadando/Beadando/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Beadando.Entities;
 
 namespace Beadando
 {
     public partial class Form1 : Form
     {
+        List<Expense> Expenses = new List<Expense>();
+        List<Income> Incomes = new List<Income>();
+
         public Form1()
         {
             InitializeComponent();

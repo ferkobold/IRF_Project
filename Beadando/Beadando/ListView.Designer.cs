954417dbe606ff3368fca8d4f3ccd91c318f381e
ferkobold <barta.ferenc98@gmail.com>
2020-12-12 20:12:21 +0100

    Starting to work on ListView form

--- Beadando/Beadando/ListView.Designer.cs
+++ Beadando/Beadando/ListView.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonYearSelect = new System.Windows.Forms.Button();
+            this.radioIncomes = new System.Windows.Forms.RadioButton();
+            this.radioExpenses = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(161, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(627, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonYearSelect
+            // 
+            this.buttonYearSelect.Location = new System.Drawing.Point(13, 96);
+            this.buttonYearSelect.Name = "buttonYearSelect";
+            this.buttonYearSelect.Size = new System.Drawing.Size(142, 35);
+            this.buttonYearSelect.TabIndex = 2;
+            this.buttonYearSelect.Text = "button1";
+            this.buttonYearSelect.UseVisualStyleBackColor = true;
+            this.buttonYearSelect.Click += new System.EventHandler(this.ButtonYearSelect_Click);
+            // 
+            // radioIncomes
+            // 
+            this.radioIncomes.AutoSize = true;
+            this.radioIncomes.Location = new System.Drawing.Point(12, 12);
+            this.radioIncomes.Name = "radioIncomes";
+            this.radioIncomes.Size = new System.Drawing.Size(65, 17);
+            this.radioIncomes.TabIndex = 5;
+            this.radioIncomes.TabStop = true;
+            this.radioIncomes.Text = "Incomes";
+            this.radioIncomes.UseVisualStyleBackColor = true;
+            // 
+            // radioExpenses
+            // 
+            this.radioExpenses.AutoSize = true;
+            this.radioExpenses.Location = new System.Drawing.Point(12, 35);
+            this.radioExpenses.Name = "radioExpenses";
+            this.radioExpenses.Size = new System.Drawing.Size(71, 17);
+            this.radioExpenses.TabIndex = 6;
+            this.radioExpenses.TabStop = true;
+            this.radioExpenses.Text = "Expenses";
+            this.radioExpenses.UseVisualStyleBackColor = true;
+            // 
+            // ListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioExpenses);
+            this.Controls.Add(this.radioIncomes);
+            this.Controls.Add(this.buttonYearSelect);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ListView";
             this.Text = "ListView";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonYearSelect;
+        private System.Windows.Forms.RadioButton radioIncomes;
+        private System.Windows.Forms.RadioButton radioExpenses;
     }
 }
--- Beadando/Beadando/ListView.cs
+++ Beadando/Beadando/ListView.cs
@@ -3,18 +3,83 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Beadando.Entities;
 
 namespace Beadando
 {
     public partial class ListView : Form
     {
+        List<Expense> Expenses = new List<Expense>();
+        List<Income> Incomes = new List<Income>();
+        string path = "C:\\temp\\";
+        string filename = "2020";
         public ListView()
         {
             InitializeComponent();
+            Expenses = GetExpenses();
+            Incomes = GetIncomes(@"C:\temp\Incomes.csv");
+        }
+
+
+        //---CREATING INCOME/EXPENSE LISTS---//
+        public List<Expense> GetExpenses(string csvpath)
+        {
+            List<Expense> expenses = new List<Expense>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    expenses.Add(new Expense()
+                    {
+                        Month = (Month)Enum.Parse(typeof(Month), line[0]),
+                        Expensetype = (Expensetype)Enum.Parse(typeof(Expensetype), line[1]),
+                        Value = int.Parse(line[2])
+                    });
+                }
+            }
+            return expenses;
+        }
+
+        public List<Income> GetIncomes(string csvpath)
+        {
+            List<Income> incomes = new List<Income>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    incomes.Add(new Income()
+                    {
+                        Month = (Month)Enum.Parse(typeof(Month), line[0]),
+                        Incometype = (Incometype)Enum.Parse(typeof(Incometype), line[1]),
+                        Value = int.Parse(line[2])
+                    });
+                }
+            }
+            return incomes;
+        }
+
+        private void ButtonYearSelect_Click(object sender, EventArgs e)
+        {
+            /*Ez a kód itt beállítja a fájlnevet .csv befejezéssel, hogy ki tudja olvasni a GetIncome, Expense függvény*/
+            if (radioIncomes.Checked)
+            {
+                path = "C:\\temp\\Incomes\\";
+            }
+
+            if (radioExpenses.Checked)
+            {
+                path = "C:\\temp\\Expenses\\";
+            }
+            filename = textBox1.Text + ".csv";
         }
     }
 }

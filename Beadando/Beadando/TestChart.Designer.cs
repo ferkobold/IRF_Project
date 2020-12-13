0d7c554f102761f001ee40d06616ece49bc7312c
ferkobold <barta.ferenc98@gmail.com>
2020-12-13 15:02:15 +0100

    Working TestChart, starting to work on linQ values for the chart

--- Beadando/Beadando/TestChart.Designer.cs
+++ Beadando/Beadando/TestChart.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioExpenses = new System.Windows.Forms.RadioButton();
+            this.radioIncomes = new System.Windows.Forms.RadioButton();
+            this.buttonYearSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(329, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(459, 426);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // radioExpenses
+            // 
+            this.radioExpenses.AutoSize = true;
+            this.radioExpenses.Location = new System.Drawing.Point(12, 32);
+            this.radioExpenses.Name = "radioExpenses";
+            this.radioExpenses.Size = new System.Drawing.Size(71, 17);
+            this.radioExpenses.TabIndex = 18;
+            this.radioExpenses.TabStop = true;
+            this.radioExpenses.Text = "Expenses";
+            this.radioExpenses.UseVisualStyleBackColor = true;
+            this.radioExpenses.CheckedChanged += new System.EventHandler(this.RadioExpenses_CheckedChanged);
+            // 
+            // radioIncomes
+            // 
+            this.radioIncomes.AutoSize = true;
+            this.radioIncomes.Location = new System.Drawing.Point(12, 9);
+            this.radioIncomes.Name = "radioIncomes";
+            this.radioIncomes.Size = new System.Drawing.Size(65, 17);
+            this.radioIncomes.TabIndex = 17;
+            this.radioIncomes.TabStop = true;
+            this.radioIncomes.Text = "Incomes";
+            this.radioIncomes.UseVisualStyleBackColor = true;
+            this.radioIncomes.CheckedChanged += new System.EventHandler(this.RadioIncomes_CheckedChanged);
+            // 
+            // buttonYearSelect
+            // 
+            this.buttonYearSelect.Location = new System.Drawing.Point(12, 83);
+            this.buttonYearSelect.Name = "buttonYearSelect";
+            this.buttonYearSelect.Size = new System.Drawing.Size(142, 35);
+            this.buttonYearSelect.TabIndex = 16;
+            this.buttonYearSelect.Text = "button1";
+            this.buttonYearSelect.UseVisualStyleBackColor = true;
+            this.buttonYearSelect.Click += new System.EventHandler(this.ButtonYearSelect_Click);
+            // 
+            // TestChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioExpenses);
+            this.Controls.Add(this.radioIncomes);
+            this.Controls.Add(this.buttonYearSelect);
+            this.Controls.Add(this.chart1);
+            this.Name = "TestChart";
             this.Text = "TestChart";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioExpenses;
+        private System.Windows.Forms.RadioButton radioIncomes;
+        private System.Windows.Forms.Button buttonYearSelect;
     }
 }
--- Beadando/Beadando/TestChart.cs
+++ Beadando/Beadando/TestChart.cs
@@ -3,18 +3,129 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Beadando.Entities;
 
 namespace Beadando
 {
     public partial class TestChart : Form
     {
+        List<Expense> Expenses = new List<Expense>();
+        List<Income> Incomes = new List<Income>();
+        public string path = (@"C:\temp\Incomes\");
+        public string filename = "2019.csv";
         public TestChart()
         {
             InitializeComponent();
+            Expenses = GetExpenses(path + filename);
+            Incomes = GetIncomes(path + filename);
+        }
+
+        //---CREATING INCOME/EXPENSE LISTS---//
+        public List<Expense> GetExpenses(string csvpath)
+        {
+            List<Expense> expenses = new List<Expense>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    expenses.Add(new Expense()
+                    {
+                        Month = (Month)Enum.Parse(typeof(Month), line[0]),
+                        Expensetype = (Expensetype)Enum.Parse(typeof(Expensetype), line[1]),
+                        Value = int.Parse(line[2])
+                    });
+                }
+            }
+            return expenses;
+        }
+
+        public List<Income> GetIncomes(string csvpath)
+        {
+            List<Income> incomes = new List<Income>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    incomes.Add(new Income()
+                    {
+                        Month = (Month)Enum.Parse(typeof(Month), line[0]),
+                        Incometype = (Incometype)Enum.Parse(typeof(Incometype), line[1]),
+                        Value = int.Parse(line[2])
+                    });
+                }
+            }
+            return incomes;
+        }
+
+
+
+
+
+        //---INTERACTIVE CHART---//
+        private void RadioIncomes_CheckedChanged(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\temp\Incomes");
+            FileInfo[] Files = d.GetFiles("*.csv");
+            comboBox1.DataSource = Files;
+            comboBox1.DisplayMember = "Name";
+            path = (@"C:\temp\Incomes\");
+        }
+
+        private void RadioExpenses_CheckedChanged(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\temp\Expenses");
+            FileInfo[] Files = d.GetFiles("*.csv");
+            comboBox1.DataSource = Files;
+            comboBox1.DisplayMember = "Name";
+            path = (@"C:\temp\Expenses\");
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filename = comboBox1.Text;
+        }
+
+        private void ButtonYearSelect_Click(object sender, EventArgs e)
+        {
+            InteractiveClick();
+        }
+
+
+
+        //---CURRENTLY WOTRKING ON LINQ CHART VALUES---//
+        private void InteractiveClick()
+        {
+            if (radioIncomes.Checked)
+            {
+                Incomes = GetIncomes(path + filename);
+                chart1.DataSource = Incomes;
+                var series = chart1.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "MonthString";
+                series.YValueMembers = "Value";
+                series.Color = Color.Red;
+            }
+
+            if (radioExpenses.Checked)
+            {
+                Expenses = GetExpenses(path + filename);
+                chart1.DataSource = Expenses;
+                var series = chart1.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "MonthString";
+                series.YValueMembers = "Value";
+                series.Color = Color.Red;
+            }
         }
     }
 }
